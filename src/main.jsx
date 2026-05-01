5d2c724a1d9222cac564e2aa956cbb6dc7fb5eaf
ebelious <belanger.eric.m@gmail.com>
2026-05-01 14:42:17 -0400

    Initialize React application in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'   // you can leave this or delete if you don't need it
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
